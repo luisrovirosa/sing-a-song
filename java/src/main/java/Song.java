fed92ed94e5d35a2c55a3fe19ec212c572893d6d
Luis Rovirosa Mairlot <luis@codium.team>
2021-02-10 20:09:03 +0100

    fix shallowToCatchPhrase

--- java/src/main/java/Song.java
+++ java/src/main/java/Song.java
@@ -15,7 +15,7 @@ class Song {
                 "\n" +
                 "There was an old lady who swallowed a " + animal2 + ";\n" +
                 "That wriggled and wiggled and tickled inside her.\n" +
-                shallowToCatchPhrase(animal2, animal1) +
+                shallowToCatchPhrase(animal2, animal1) + ";\n" +
                 "I don't know why she swallowed a " + animal1 + " - perhaps she'll die!\n" +
                 "\n" +
                 "There was an old lady who swallowed a " + animal3 + ";\n" +
@@ -24,17 +24,17 @@ class Song {
                 "She swallowed the " + animal2 + " to catch the " + animal1 + ";\n" +
                 "I don't know why she swallowed a " + animal1 + " - perhaps she'll die!\n" +
                 "\n" +
-                "There was an old lady who swallowed a " + animal4 +";\n" +
-                "Fancy that to swallow a " + animal4 +"!\n" +
-                "She swallowed the " + animal4 +" to catch the " + animal3 + ",\n" +
+                "There was an old lady who swallowed a " + animal4 + ";\n" +
+                "Fancy that to swallow a " + animal4 + "!\n" +
+                "She swallowed the " + animal4 + " to catch the " + animal3 + ",\n" +
                 "She swallowed the " + animal3 + " to catch the " + animal2 + ",\n" +
                 "She swallowed the " + animal2 + " to catch the " + animal1 + ";\n" +
                 "I don't know why she swallowed a " + animal1 + " - perhaps she'll die!\n" +
                 "\n" +
                 "There was an old lady who swallowed a " + animal5 + ";\n" +
                 "What a hog, to swallow a " + animal5 + "!\n" +
-                "She swallowed the " + animal5 + " to catch the " + animal4 +",\n" +
-                "She swallowed the " + animal4 +" to catch the " + animal3 + ",\n" +
+                "She swallowed the " + animal5 + " to catch the " + animal4 + ",\n" +
+                "She swallowed the " + animal4 + " to catch the " + animal3 + ",\n" +
                 "She swallowed the " + animal3 + " to catch the " + animal2 + ",\n" +
                 "She swallowed the " + animal2 + " to catch the " + animal1 + ";\n" +
                 "I don't know why she swallowed a " + animal1 + " - perhaps she'll die!\n" +
@@ -42,8 +42,8 @@ class Song {
                 "There was an old lady who swallowed a " + animal6 + ";\n" +
                 "I don't know how she swallowed a " + animal6 + "!\n" +
                 "She swallowed the " + animal6 + " to catch the " + animal5 + ",\n" +
-                "She swallowed the " + animal5 + " to catch the " + animal4 +",\n" +
-                "She swallowed the " + animal4 +" to catch the " + animal3 + ",\n" +
+                "She swallowed the " + animal5 + " to catch the " + animal4 + ",\n" +
+                "She swallowed the " + animal4 + " to catch the " + animal3 + ",\n" +
                 "She swallowed the " + animal3 + " to catch the " + animal2 + ",\n" +
                 "She swallowed the " + animal2 + " to catch the " + animal1 + ";\n" +
                 "I don't know why she swallowed a " + animal1 + " - perhaps she'll die!\n" +
@@ -54,7 +54,7 @@ class Song {
         System.out.println(song);
     }
 
-    private static String shallowToCatchPhrase(String animal2, String animal0) {
-        return "She swallowed the " + animal2 + " to catch the " + animal0 + ";\n";
+    private static String shallowToCatchPhrase(String animalSwallowed, String animalToCatch) {
+        return "She swallowed the " + animalSwallowed + " to catch the " + animalToCatch;
     }
 }
